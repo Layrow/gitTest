a0b479a6460431988f82fbec1887a212253e5bec
fengyh <1105859440@qq.com>
2022-10-27 23:51:59 +0800

    新创建类

---
+++ src/main/java/com/sxd/TestGit.java
@@ -0,0 +1,13 @@
+package com.sxd;
+
+/**
+ * @ClassName: TestGit
+ * @Description:
+ * @Author: 冯永辉
+ * @Date: 2022/10/27 23:49
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
